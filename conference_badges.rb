ec5616dc27337eb702aa315d88937f6d2dcf11a6
Kenton Archer <kenton8654@yahoo.com>
2019-04-12 18:05:24 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,65 @@
-# Write your code here.
+require'pry'
+
+def badge_maker(name)
+
+  return "Hello, my name is #{name}."
+  #binding.pry
+end
+#   new_badge = []
+#    array.each do |name|
+#     name += 1
+#
+#    end
+#     new_badge.push("Hello, my name is #{array}")
+#
+#     puts new_badge
+#     binding.pry
+# end
+
+
+def batch_badge_creator (array)
+  messages = []
+  array. each do |i|
+   messages.push("Hello, my name is #{i}.")
+  end
+  return messages
+end
+
+
+def assign_rooms(rooms)
+
+  rooms.each_with_index.map do |obj, idx|
+("Hello, #{obj}! You'll be assigned to room #{idx += 1}!")
+    # puts obj
+    # puts idx
+  end
+
+end
+
+def printer (array)
+  batch_badge_creator(array).each do |i|
+    puts i
+     end
+  assign_rooms(array).each do |i|
+    puts i
+    end    
+end
+
+
+
+
+
+#    array.each {|i| puts "Hello my name is #{i}"}
+#      messages.push(array)
+#   end
+#
+#   return messages
+#
+#
+# end
+
+
+# I need to iterate over an array
+#
+# I need to push a string onto an empty array
+#
